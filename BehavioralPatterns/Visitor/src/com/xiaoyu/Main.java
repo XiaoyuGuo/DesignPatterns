33f30a8f4c3105739506639b59a2eac9f4f8f366
XiaoyuGuo <16126144@bjtu.edu.cn>
2016-10-31 21:22:47 +0800

    final

--- BehavioralPatterns/Visitor/src/com/xiaoyu/Main.java
+++ BehavioralPatterns/Visitor/src/com/xiaoyu/Main.java
@@ -2,11 +2,12 @@ package com.xiaoyu;
 
 /**
  * Created by gxytk on 2016/10/31.
- * 观光者模式
+ * 访问者模式
  * CarElementPrintVisitor实现了抽象车辆元素观光者的接口，
  * 实现了具体的对于每一个车辆元素的观光方法
  * 在这里的观光方法主要是print
  * 可以实现具体算法与元素的剥离
+ * 适用于稳定的数据结构和易变的操作
  */
 public class Main {
     public static void main(String[] args) {
